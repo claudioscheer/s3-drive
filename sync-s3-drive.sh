8b212be5d6af850cad5cec39f7230567661d4fce
Claudio Scheer <claudioscheer@protonmail.com>
2020-01-15 00:42:36 -0300

    Moved to aws2 and removed conda

--- sync-s3-drive.sh
+++ sync-s3-drive.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate aws
-
 S3_PATH=~/s3-drive
 
 if [ "$1" == "push" ]; then
 	date +%s > $S3_PATH/.last_push
-	aws s3 sync $S3_PATH s3://s3-drive-43e3d5 --profile s3-drive $2
+	aws2 s3 sync $S3_PATH s3://s3-drive-43e3d5 --profile s3-drive $2
 else
-	aws s3 sync s3://s3-drive-43e3d5 $S3_PATH --profile s3-drive $2
+	aws2 s3 sync s3://s3-drive-43e3d5 $S3_PATH --profile s3-drive $2
 fi
